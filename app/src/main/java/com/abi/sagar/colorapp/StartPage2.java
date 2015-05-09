9248831018e147c1734c68ecdc72fd48651e4cc1
sagargarg <sagargarg@gmail.com>
2015-05-09 12:32:37 -0400

    Triangle rotates when screen is clicked

--- app/src/main/java/com/abi/sagar/colorapp/StartPage2.java
+++ app/src/main/java/com/abi/sagar/colorapp/StartPage2.java
@@ -1,21 +1,23 @@
 package com.abi.sagar.colorapp;
 
-import android.content.Context;
-import android.content.DialogInterface;
+import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
-import android.app.Activity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 public class StartPage2 extends Activity {
 
     AnimationDrawable rocketAnimation;
+    Animation rotate1;
+    Animation rotate2;
+    Animation rotate3;
+    View triangle2;
+    int currentAnimation = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,35 +27,37 @@ public class StartPage2 extends Activity {
         Animation rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
         findViewById(R.id.triangle1).startAnimation(rotate);
 
-        final Animation rotate1 = AnimationUtils.loadAnimation(this, R.anim.rotate1);
-        final Animation rotate2 = AnimationUtils.loadAnimation(this, R.anim.rotate2);
-        final Animation rotate3 = AnimationUtils.loadAnimation(this, R.anim.rotate3);
-        final View Tri2 = (View) findViewById(R.id.triangle2);
-
-    
-
-        Tri2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Tri2.startAnimation(rotate1);
-                rotate1.start();
-
-                Tri2.startAnimation(rotate2);
-                rotate2.start();
-
-                Tri2.startAnimation(rotate3);
-                rotate3.start();
-
-            }
-        });
-
+        rotate1 = AnimationUtils.loadAnimation(this, R.anim.rotate1);
+        rotate2 = AnimationUtils.loadAnimation(this, R.anim.rotate2);
+        rotate3 = AnimationUtils.loadAnimation(this, R.anim.rotate3);
+        triangle2 = (View) findViewById(R.id.triangle2);
 
         ImageView rocketImage = (ImageView) findViewById(R.id.moving_red_box);
         rocketImage.setBackgroundResource(R.drawable.animation_box);
         rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
         rocketAnimation.start();
+
     }
 
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            switch(++currentAnimation){
+                case 1:
+                    triangle2.startAnimation(rotate1);
+                    break;
+                case 2:
+                    triangle2.startAnimation(rotate2);
+                    break;
+                case 3:
+                    currentAnimation = 0;
+                    triangle2.startAnimation(rotate3);
+                    break;
+            }
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
 
 
     @Override
@@ -62,13 +66,4 @@ public class StartPage2 extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
-    // This code makes the animation start after user clicks on the screen
-    /*public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            rocketAnimation.start();
-            return true;
-        }
-        return super.onTouchEvent(event);
-    }*/
 }
